c65fc734ec0a9ddcfd2981df7eecdf13d948a212
Mikhail Tolstoy <mikhail.tolstoy@onefactor.com>
2022-01-27 10:02:44 +0300

    working random bot

--- build.sbt
+++ build.sbt
@@ -8,7 +8,13 @@ ThisBuild / organizationName := "example"
 lazy val root = (project in file("."))
   .settings(
     name := "multi-layer-diary-bot",
-    libraryDependencies += scalaTest % Test
+    libraryDependencies ++= Seq(
+      scalaTest % Test,
+      "com.bot4s" %% "telegram-core" % "5.3.0",
+      "com.bot4s" %% "telegram-akka" % "5.3.0",
+      "com.softwaremill.sttp.client3" %% "core" % "3.4.1",
+      "com.softwaremill.sttp.client3" %% "okhttp-backend" % "3.4.1"
+    )
   )
 
 // See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,9 +1,87 @@
 package example
 
+import cats.instances.future._
+import cats.syntax.functor._
+import com.bot4s.telegram.api.RequestHandler
+import com.bot4s.telegram.api.declarative.Commands
+import com.bot4s.telegram.clients.FutureSttpClient
+import com.bot4s.telegram.future.{Polling, TelegramBot}
+import com.bot4s.telegram.methods.SendDice
+import sttp.client3._
+import sttp.client3.okhttp.OkHttpFutureBackend
+
+import scala.concurrent.Await
+import scala.util.Try
+import scala.concurrent.Future
+import scala.concurrent.duration.Duration
+
 object Hello extends Greeting with App {
-  println(greeting)
+  val bot = new RandomBot("")
+  val eol = bot.run()
+  println("Press [ENTER] to shutdown the bot, it may take a few seconds...")
+  scala.io.StdIn.readLine()
+  bot.shutdown() // initiate shutdown
+  // Wait for the bot end-of-life
+  Await.result(eol, Duration.Inf)
 }
 
 trait Greeting {
   lazy val greeting: String = "hello"
+}
+
+class RandomBot(token: String) extends ExampleBot(token) with Polling with Commands[Future] {
+
+  val rng = new scala.util.Random(System.currentTimeMillis())
+  onCommand("coin" or "flip") { implicit msg =>
+    reply(if (rng.nextBoolean()) "Head!" else "Tail!").void
+  }
+  onCommand("real" | "double" | "float") { implicit msg =>
+    reply(rng.nextDouble().toString).void
+  }
+  onCommand("/die" | "roll") { implicit msg =>
+    reply("⚀⚁⚂⚃⚄⚅" (rng.nextInt(6)).toString).void
+  }
+  onCommand("random" or "rnd") { implicit msg =>
+    withArgs {
+      case Seq(Int(n)) if n > 0 =>
+        reply(rng.nextInt(n).toString).void
+      case _ => reply("Invalid argumentヽ(ಠ_ಠ)ノ").void
+    }
+  }
+  onCommand("choose" | "pick" | "select") { implicit msg =>
+    withArgs { args =>
+      replyMd(if (args.isEmpty) "No arguments provided." else args(rng.nextInt(args.size))).void
+    }
+  }
+
+  onCommand("auto") { implicit msg =>
+    request(SendDice(msg.chat.id)).void
+  }
+  // Extractor
+  object Int {
+    def unapply(s: String): Option[Int] = Try(s.toInt).toOption
+  }
+
+}
+
+import com.bot4s.telegram.api.RequestHandler
+import com.bot4s.telegram.clients.FutureSttpClient
+import com.bot4s.telegram.future.TelegramBot
+
+import scala.concurrent.Future
+
+/**
+ * Quick helper to spawn example bots.
+ *
+ * Mix Polling or Webhook accordingly.
+ *
+ * Example:
+ * new EchoBot(":").run()
+ *
+ * @param token Bot's token.
+ */
+abstract class ExampleBot(val token: String) extends TelegramBot {
+
+  implicit val backend                        = OkHttpFutureBackend()
+  override val client: RequestHandler[Future] = new FutureSttpClient(token)
 }
